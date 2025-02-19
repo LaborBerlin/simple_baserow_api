bdc4b0b8d108dbf0a0ab319df091f20f07f2054f
Markus Konrad <markus.konrad@laborberlin.com>
2025-02-19 11:40:01 +0100

    WIP: create_table command

--- simple_baserow_api/__main__.py
+++ simple_baserow_api/__main__.py
@@ -1,6 +1,19 @@
 """Entry point for simple_baserow_api."""
+import argparse
 
 from simple_baserow_api.cli import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    args = argparse.ArgumentParser('python -m simple_baserow_api')
+    args.add_argument('--url')
+    args.add_argument('--token')
+    args.add_argument('--user')
+    args.add_argument('--passwd')
+    subargs = args.add_subparsers(dest='command', required=True)
+
+    create_table_args = subargs.add_parser('create_table')
+    create_table_args.add_argument('database_id', type=int)
+    create_table_args.add_argument('table_name')
+    create_table_args.add_argument('--fields_schema')
+
+    main(args.parse_args())
--- simple_baserow_api/cli.py
+++ simple_baserow_api/cli.py
@@ -1,21 +1,52 @@
 """CLI interface for simple_baserow_api project.
 """
 
+import os
+import sys
+import json
+from dataclasses import fields
 
-def main():  # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m simple_baserow_api` and `$ simple_baserow_api `.
+from simple_baserow_api import BaserowApi
 
-    This is your program's entry point.
 
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
-    print("This will do something")
+def fail_if_missing_arg(argname, argv):
+    if argv is None:
+        print(f'Required baserow API {argname} not given.', file=sys.stderr)
+        sys.exit(1)
+
+
+class Commands:
+    api = None    # type: BaserowApi
+    user = None
+    passwd = None
+
+    @classmethod
+    def create_table(cls, args):
+        fail_if_missing_arg('user', cls.user)
+        fail_if_missing_arg('password', cls.passwd)
+
+        if args.fields_schema:
+            with open(args.fields_schema) as f:
+                fields = json.load(f)
+        else:
+            fields = None
+
+        cls.api.get_jwt(cls.user, cls.passwd)
+        cls.api.create_table(args.database_id, args.table_name, fields)
+
+
+def main(args):
+    cmd = args.command
+
+    baseurl = args.url or os.environ.get('BASEROWAPI_URL', None)
+    fail_if_missing_arg('URL', baseurl)
+
+    token = args.url or os.environ.get('BASEROWAPI_TOKEN', None)
+    fail_if_missing_arg('Token', token)
+
+    Commands.user = args.user or os.environ.get('BASEROWAPI_USER', None)
+    Commands.passwd = args.passwd or os.environ.get('BASEROWAPI_PASSWD', None)
+    Commands.api = BaserowApi(baseurl, token)
+
+    method = getattr(Commands, cmd)
+    method(args)
